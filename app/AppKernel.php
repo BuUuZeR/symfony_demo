3669b32afe2e5615cd5d114d15731e5e20c8644b
Ivan <ivan@media-events.mc>
2016-09-06 16:10:53 +0200

    Generation du bundle SfWebApp/FrontOfficeBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new SfWebApp\MainBundle\SfWebAppMainBundle(),
             new SfWebApp\BackOfficeBundle\SfWebAppBackOfficeBundle(),
+            new SfWebApp\FrontOfficeBundle\SfWebAppFrontOfficeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
