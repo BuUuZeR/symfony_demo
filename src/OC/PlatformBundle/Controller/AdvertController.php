36a8e354545d033bd06db66acb1af11bf0aea8da
Ivan <ivan@media-events.mc>
2016-09-07 16:51:29 +0200

    Mise en place du controller Advert - Chapitre 8

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,24 +5,53 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
-	public function indexAction()
+	public function indexAction($page)
 	{
-		$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Ivan'));
+		if($page < 1)
+		{
+			throw new NotFoundHttpException('Page '.$page.' inexistante');
+		} 
 
-		return new Response($content);
+		return $this->render('OCPlatformBundle:Advert:index.html.twig');
 	}
 
 	public function viewAction($id)
 	{
-		return new Response('Parametre passé par url '.$id);
+		return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+			'id' => $id,
+		));
 	}
 
-	public function viewSlugAction($slug,$year,$format)
+	public function addAction(Request $request)
 	{
-		return new Response('Slug : '.$slug.'<br>L\'année '.$year.'<br>Format : '.$format);
+		if($request->isMethod('POST'))
+		{
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistré');
+
+			return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+		}
+
+		return $this->render('OCPlatformBundle:Advert:add.html.twig');
 	}
+
+	public function editAction($id, Request $request)
+	{
+		if($request->isMethod('POST'))
+		{
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifié');
+		}
+
+		return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+	}
+
+	public function deleteAction($id)
+	{
+		return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+	}
+
 }
