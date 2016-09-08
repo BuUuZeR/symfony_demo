85b0d7167e30e4f375ae2c6a2e55bb57c230ddbe
Ivan <ivan@media-events.mc>
2016-09-08 10:57:10 +0200

    Mise enplace du CoreBundle

--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -5,6 +5,17 @@ namespace OC\PlatformBundle\Antispam;
 
 class OCAntispam
 {
+	private $mailer;
+	private $locale;
+	private $minLength;
+
+	public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+	{
+		$this->mailer = $mailer;
+		$this->locale = $locale;
+		$this->minLegth = (int) $minLength;
+	}
+
 	public function isSpam($text)
 	{
 		return strlen($text) < 50;
